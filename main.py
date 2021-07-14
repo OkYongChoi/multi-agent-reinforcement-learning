d3c727ff0cd4f1ff37dae3a4f2f1415db6cf2fd7
burger-okd <ok9623@gmail.com>
2021-07-15 00:00:47 +0900

    add file reader writer

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #%%
-import numpy as np
 import torch
-
-from qnet import QNet
 from hyst_agent import HystAgent
-
 import matplotlib.pyplot as plt
+import pickle
+import csv
+
+import time
 # %%
 ##
 ACTION_SIZE = 5 # Dimension of action space of each agent.
@@ -24,8 +24,8 @@ print('device: ', device)
 #Make random reward array
 #This is a shared reward setting, not an individualized reward setting 
 # reward_matrix = np.random.randint(low = -REWARD_BOUND,
-#                                  high = REWARD_BOUND,
-#                                  size=(ACTION_SIZE, ACTION_SIZE))
+#                                   high = REWARD_BOUND,
+#                                   size = (ACTION_SIZE, ACTION_SIZE))
 reward_matrix=[[11, -30, 0], 
                [-30, 7, 6], 
                [0, 0, 5]]
@@ -34,11 +34,15 @@ if __name__ == "__main__":
     # The state of the game is only one and state is not changed
     state = torch.Tensor([1]).to(device)
     next_state = torch.Tensor([1]).to(device)
-    agent1 = HystAgent()
-    agent2 = HystAgent()
+
+    # 2-agent matrix game
+    # If you want to set lambda same with gamma, set lambda 0.99
+    agent1 = HystAgent(lamb=0.1, increase_lr=2, decrease_lr=0.2)
+    agent2 = HystAgent(lamb=0.1, increase_lr=2, decrease_lr=0.2)
 
     reward_list = []
-
+    
+    start = time.time()
     for i in range(3000):
         act_agent1 = agent1.get_action(state)
         act_agent2 = agent2.get_action(state)
@@ -53,6 +57,15 @@ if __name__ == "__main__":
         #train the model of each agent.
         agent1.train_model()
         agent2.train_model()
+    print(f"time:{time.time() - start}")
+    
+    # with open('reward_list.pkl','wb') as f:
+    #     pickle.dump(reward_list, f)
+    with open('reward_list.csv','w') as f:
+        writer = csv.writer(f)
+        writer.writerow(reward_list)
+    agent1.save_model('hyst_1')
+    agent2.save_model('hyst_2')
 
     plt.plot(reward_list)
     plt.show()
